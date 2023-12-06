cd4348d08fd8d6e07be33b1a4a8a4de332d24b95
Jickx <valentin.sharonov@gmail.com>
2023-12-06 20:40:20 +0300

    day 2 part 2

--- 2023/day_02.py
+++ 2023/day_02.py
@@ -1,6 +1,7 @@
 """
 Advent of Code 2023 - Day 02 (https://adventofcode.com/2023/day/2)
 """
+from collections import defaultdict
 
 MAX_CUBES = {
     'red': 12,
@@ -9,8 +10,8 @@ MAX_CUBES = {
 }
 
 
-def is_set_valid(cubes_set):
-    for _set in cubes_set:
+def is_set_valid(sets):
+    for _set in sets:
         cubes = _set.split(',')
         for cube in cubes:
             cubes_split = cube.strip(' ').split(' ')
@@ -20,13 +21,7 @@ def is_set_valid(cubes_set):
     return True
 
 
-def count_games():
-    with open('input/day_02.txt', 'r') as f:
-        data = []
-        for line in f:
-            data.append(line.strip('\n'))
-
-    data_dict = {}
+def count_games_part_1(data):
     result = 0
     for line in data:
         line_split = line.split(':')
@@ -35,8 +30,43 @@ def count_games():
         set_valid = is_set_valid(sets)
         if set_valid:
             result += int(game_num)
-
     return result
 
 
-print(count_games())
+def find_fewest_games(sets):
+    games_list = []
+    for _set in sets:
+        cubes_dict = {}
+        cubes = _set.split(',')
+        for cube in cubes:
+            cubes_split = cube.strip(' ').split(' ')
+            cubes_num, cubes_color = cubes_split[0], cubes_split[1]
+            cubes_dict[cubes_color] = cubes_num
+        games_list.append(cubes_dict)
+
+    games_dict = defaultdict(int)
+    for game in games_list:
+        for k, v in game.items():
+            games_dict[k] = max(games_dict[k], int(v))
+    result = 1
+    for v in games_dict.values():
+        result *= int(v)
+    return result
+
+
+def count_games_part_2(data):
+    result = 0
+    for line in data:
+        line_split = line.split(':')
+        sets = line_split[1].split(';')
+        result += find_fewest_games(sets)
+    return result
+
+
+with open('input/day_02.txt', 'r') as f:
+    data = []
+    for line in f:
+        data.append(line.strip('\n'))
+
+assert count_games_part_1(data) == 2727
+assert count_games_part_2(data) == 56580
