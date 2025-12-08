25b0995ddcceab1931d3316a10d2d12590113a6a
Jickx <valentin.sharonov@gmail.com>
2025-12-08 09:59:31 +0300

    year 2015 day 1 part 2

--- 2015/day_01.py
+++ 2015/day_01.py
@@ -1,25 +1,38 @@
-query = open('input/day_01.txt', 'r').read()
-lines = query.split('\n')
-
-def find_floor(sequence: list) -> int:
-    result = 0
-    for line in sequence:
-        for ch in line:
-            if ch == "(":
-                result +=1
-            elif ch == ")":
-                result -=1
-    return result
+with open('input/day_01.txt', 'r') as f:
+    query = f.read().strip()
 
 
-assert find_floor(["(())"]) == 0
-assert find_floor("()()") == 0
-assert find_floor("(((") == 3
-assert find_floor("(()(()(") == 3
-assert find_floor("))(((((") == 3
-assert find_floor("())") == -1
-assert find_floor("))(") == -1
-assert find_floor(")))") == -3
-assert find_floor(")())())") == -3
+def find_floor_part_1(sequence: str) -> int:
+    floor = 0
+    for ch in sequence:
+        if ch == "(":
+            floor += 1
+        elif ch == ")":
+            floor -= 1
+    return floor
 
-print(find_floor(lines))
+
+def find_floor_part_2(sequence: str) -> int:
+    floor = 0
+    for i, ch in enumerate(sequence, start=1):
+        if ch == "(":
+            floor += 1
+        elif ch == ")":
+            floor -= 1
+        if floor == -1:
+            return i
+    return -1
+
+
+assert find_floor_part_1("(())") == 0
+assert find_floor_part_1("()()") == 0
+assert find_floor_part_1("(((") == 3
+assert find_floor_part_1("(()(()(") == 3
+assert find_floor_part_1("))(((((") == 3
+assert find_floor_part_1("())") == -1
+assert find_floor_part_1("))(") == -1
+assert find_floor_part_1(")))") == -3
+assert find_floor_part_1(")())())") == -3
+
+print(find_floor_part_1(query))
+print(find_floor_part_2(query))
